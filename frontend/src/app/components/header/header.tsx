c5ce9e900afd18c0cb0dac19b02a3e2c6e62e95e
Luiz Antonio Ponciano Costa Bezerra <luiz.bezerra@goesnicoladelli.net>
2024-11-05 16:00:13 -0300

    Adicionado o href no btn da loja

--- frontend/src/app/components/header/header.tsx
+++ frontend/src/app/components/header/header.tsx
@@ -1,21 +1,29 @@
-'use client'
+"use client";
 
-import { HeaderStyle, Logo, ContainerButtonShop, ShopImage, ShopText } from "./style"
+import {
+  HeaderStyle,
+  Logo,
+  ContainerButtonShop,
+  ShopImage,
+  ShopText,
+} from "./style";
 
 export default function Header() {
   return (
     <>
       <HeaderStyle>
-        <div style={{ display: "flex", alignItems: "baseline", gap: "5%"}}>
+        <div style={{ display: "flex", alignItems: "baseline", gap: "5%" }}>
           <Logo>HBL</Logo>
-          <h2 style={{color: "#fff"}}>OFERTAS</h2>
+          <h2 style={{ color: "#fff" }}>OFERTAS</h2>
         </div>
 
-        <ContainerButtonShop>
-          <ShopText>VISITE NOSSA LOJA</ShopText>
-          <ShopImage src="/shop.svg" alt="shop"/>
-        </ContainerButtonShop>
+        <a style={{ textDecoration: "none" }} href="https://hblvendas.com.br/" target="blank">
+          <ContainerButtonShop>
+            <ShopText>VISITE NOSSA LOJA</ShopText>
+            <ShopImage src="/shop.svg" alt="shop" />
+          </ContainerButtonShop>
+        </a>
       </HeaderStyle>
     </>
-  )
+  );
 }
