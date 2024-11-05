3b27a2f09d2f2edd9d353c90e3e01584a7ead220
Luiz Antonio Ponciano Costa Bezerra <luiz.bezerra@goesnicoladelli.net>
2024-11-05 15:39:11 -0300

    Logo da Header feita

--- frontend/src/app/components/header/header.tsx
+++ frontend/src/app/components/header/header.tsx
@@ -1,12 +1,15 @@
 'use client'
 
-import { HeaderStyle } from "./style"
+import { HeaderStyle, Logo } from "./style"
 
 export default function Header() {
   return (
     <>
       <HeaderStyle>
-        
+        <div style={{ display: "flex", alignItems: "baseline", gap: "5%"}}>
+          <Logo>HBL</Logo>
+          <h2 style={{color: "#fff"}}>OFERTAS</h2>
+        </div>
       </HeaderStyle>
     </>
   )
--- frontend/src/app/components/header/style.ts
+++ frontend/src/app/components/header/style.ts
@@ -2,9 +2,20 @@ import styled from "styled-components";
 import { colors } from "@/app/styles/colors";
 
 export const HeaderStyle = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
   top: 0;
   width: 100%;
   height: 100px;
+  padding: 0 20px 0 20px;
+ 
   border-radius: 0 0 20px 20px;
   background-color: ${colors["green-200"]};
+`;
+
+export const Logo = styled.h1`
+  font-size: 80px;
+  letter-spacing: -5px;
+  color: ${colors.white};
 `;
