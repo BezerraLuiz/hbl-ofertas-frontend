d63924df9d558be6cc8e16cd0d14fa5ed51906e9
Luiz Antonio Ponciano Costa Bezerra <luiz.bezerra@goesnicoladelli.net>
2024-11-06 11:44:17 -0300

    Iniciado page login

---
+++ frontend/src/app/pages/auth/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import { Body } from "./style"
+
+export default function Login() {
+  return (
+    <>
+      <Body></Body>
+    </>
+  )
+}
---
+++ frontend/src/app/pages/auth/style.ts
@@ -0,0 +1,6 @@
+import { colors } from "@/app/styles/colors";
+import styled from "styled-components";
+
+export const Body = styled.body`
+  background-color: ${colors.white};
+`;
